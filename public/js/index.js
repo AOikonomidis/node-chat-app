5c44acec8b35085f5111bacfacc5cd880661c329
Andrew Oikonomidis <andyreneyload@gmail.com>
2018-02-19 20:17:20 +0200

    emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'Mike',
-        text: 'Hey. Everything is fine down here.'
-    });
 });
 
 socket.on('disconnect', function () {
